ce8606bc38e310e8beecbd04b74335338160949c
Theresa W Chavez Theresa <49944076+wuyafeiya@users.noreply.github.com>
2023-03-10 15:18:36 +0800

    Cut

--- src/components/CutOut/Dialog.vue
+++ src/components/CutOut/Dialog.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <el-dialog title="提示" :visible="dialogVisible" width="30%" :before-close="handleClose">
+    <el-dialog title="裁剪图片" :visible="dialogVisible" width="30%" :before-close="handleClose">
       <cropper :img="img"></cropper>
       <span slot="footer" class="dialog-footer">
         <el-button @click="handleClose">取 消</el-button>
